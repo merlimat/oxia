b642e39a42871d6060e8f87219d2f2844e0136a5
Matteo Merli <mmerli@apache.org>
2025-10-16 13:07:32 -0700

    test

--- coordinator/metadata/metadata_raft.go
+++ coordinator/metadata/metadata_raft.go
@@ -15,18 +15,12 @@
 package metadata
 
 import (
-	"encoding/json"
-	"io"
 	"log/slog"
-	"net"
 	"os"
 	"path/filepath"
 	"sync"
-	"time"
-
-	"github.com/hashicorp/raft"
-	boltdb "github.com/hashicorp/raft-boltdb"
-	"github.com/magodo/slog2hclog"
+	//"github.com/hashicorp/raft"
+	//"github.com/magodo/slog2hclog"
 	"github.com/pkg/errors"
 
 	"github.com/oxia-db/oxia/coordinator/model"
@@ -46,60 +40,60 @@ func newStateContainer() *stateContainer {
 	}
 }
 
-// Apply applies a Raft log entry to the FSM.
-func (sc *stateContainer) Apply(logEntry *raft.Log) any {
-	// Each log entry contains the whole state
-	return json.Unmarshal(logEntry.Data, sc)
-}
-
-// Snapshot returns a snapshot of the FSM.
-func (sc *stateContainer) Snapshot() (raft.FSMSnapshot, error) {
-	return &stateSnapshot{
-		ClusterStatus: sc.ClusterStatus.Clone(),
-		Version:       sc.Version,
-	}, nil
-}
-
-// Restore stores the key-value pairs from a snapshot.
-func (sc *stateContainer) Restore(rc io.ReadCloser) error {
-	defer rc.Close()
-
-	dec := json.NewDecoder(rc)
-	return dec.Decode(sc)
-}
-
-type stateSnapshot struct {
-	ClusterStatus *model.ClusterStatus `json:"clusterStatus"`
-	Version       Version              `json:"version"`
-}
-
-func (s *stateSnapshot) Persist(sink raft.SnapshotSink) error {
-	value, err := json.Marshal(s)
-	if err != nil {
-		_ = sink.Cancel()
-		return err
-	}
-	if _, err := sink.Write(value); err != nil {
-		_ = sink.Cancel()
-		return err
-	}
-	return sink.Close()
-}
-
-func (*stateSnapshot) Release() {}
+//// Apply applies a Raft log entry to the FSM.
+//func (sc *stateContainer) Apply(logEntry *raft.Log) any {
+//	// Each log entry contains the whole state
+//	return json.Unmarshal(logEntry.Data, sc)
+//}
+//
+//// Snapshot returns a snapshot of the FSM.
+//func (sc *stateContainer) Snapshot() (raft.FSMSnapshot, error) {
+//	return &stateSnapshot{
+//		ClusterStatus: sc.ClusterStatus.Clone(),
+//		Version:       sc.Version,
+//	}, nil
+//}
+//
+//// Restore stores the key-value pairs from a snapshot.
+//func (sc *stateContainer) Restore(rc io.ReadCloser) error {
+//	defer rc.Close()
+//
+//	dec := json.NewDecoder(rc)
+//	return dec.Decode(sc)
+//}
+//
+//type stateSnapshot struct {
+//	ClusterStatus *model.ClusterStatus `json:"clusterStatus"`
+//	Version       Version              `json:"version"`
+//}
+//
+//func (s *stateSnapshot) Persist(sink raft.SnapshotSink) error {
+//	value, err := json.Marshal(s)
+//	if err != nil {
+//		_ = sink.Cancel()
+//		return err
+//	}
+//	if _, err := sink.Write(value); err != nil {
+//		_ = sink.Cancel()
+//		return err
+//	}
+//	return sink.Close()
+//}
+//
+//func (*stateSnapshot) Release() {}
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 type metadataProviderRaft struct {
 	sync.Mutex
 
-	sc   *stateContainer
-	raft *raft.Raft
-	log  *slog.Logger
+	sc *stateContainer
+	//raft *raft.Raft
+	log *slog.Logger
 }
 
 func (mpr *metadataProviderRaft) WaitToBecomeLeader() error {
-	<-mpr.raft.LeaderCh()
+	//<-mpr.raft.LeaderCh()
 	return nil
 }
 
@@ -120,73 +114,74 @@ func NewMetadataProviderRaft(
 		return nil, errors.Wrap(err, "failed to create data dir")
 	}
 
-	// Setup Raft configuration
-	config := raft.DefaultConfig()
-	config.HeartbeatTimeout = 5 * time.Second
-	config.ElectionTimeout = 10 * time.Second
-	config.LocalID = raft.ServerID(nodeId)
-	config.LogLevel = "INFO"
-	levelVar := &slog.LevelVar{}
-	levelVar.Set(slog.LevelInfo)
-	config.Logger = slog2hclog.New(mpr.log, levelVar)
+	//// Setup Raft configuration
+	//config := raft.DefaultConfig()
+	//config.HeartbeatTimeout = 5 * time.Second
+	//config.ElectionTimeout = 10 * time.Second
+	//config.LocalID = raft.ServerID(nodeId)
+	//config.LogLevel = "INFO"
+	//levelVar := &slog.LevelVar{}
+	//levelVar.Set(slog.LevelInfo)
+	//config.Logger = slog2hclog.New(mpr.log, levelVar)
 
-	// Create TCP transport for Raft
-	addr, err := net.ResolveTCPAddr("tcp", raftAddress)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to resolve raft address")
-	}
-	transport, err := raft.NewTCPTransport(raftAddress, addr, 3, 10*time.Second, os.Stderr)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create raft transport")
-	}
+	//// Create TCP transport for Raft
+	//addr, err := net.ResolveTCPAddr("tcp", raftAddress)
+	//if err != nil {
+	//	return nil, errors.Wrap(err, "failed to resolve raft address")
+	//}
+	//transport, err := raft.NewTCPTransport(raftAddress, addr, 3, 10*time.Second, os.Stderr)
+	//if err != nil {
+	//	return nil, errors.Wrap(err, "failed to create raft transport")
+	//}
+	//
+	//// Create snapshot store
+	//snapshotStore, err := raft.NewFileSnapshotStore(dataDir, 2, os.Stderr)
+	//if err != nil {
+	//	return nil, errors.Wrap(err, "failed to create snapshot store")
+	//}
 
-	// Create snapshot store
-	snapshotStore, err := raft.NewFileSnapshotStore(dataDir, 2, os.Stderr)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create snapshot store")
-	}
+	//// Create BoltDB-based stable store and log store
+	//boltDBPath := filepath.Join(dataDir, "raft.db")
+	//store, err := boltdb.NewBoltStore(boltDBPath)
+	//if err != nil {
+	//	return nil, errors.Wrap(err, "failed to create bolt store")
+	//}
 
-	// Create BoltDB-based stable store and log store
-	boltDBPath := filepath.Join(dataDir, "raft.db")
-	store, err := boltdb.NewBoltStore(boltDBPath)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create bolt store")
-	}
-
-	// Create Raft node
-	mpr.raft, err = raft.NewRaft(config, mpr.sc, store, store, snapshotStore, transport)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create raft node")
-	}
-
-	if hasState, err := raft.HasExistingState(store, store, snapshotStore); err != nil {
-		return nil, errors.Wrap(err, "failed to check existing state")
-	} else if !hasState {
-		configuration := raft.Configuration{
-			Servers: getRaftServers(raftBootstrapNodes),
-		}
-		future := mpr.raft.BootstrapCluster(configuration)
-		if err := future.Error(); err != nil {
-			return nil, errors.Wrap(err, "failed to create raft node")
-		}
-	}
+	//// Create Raft node
+	//mpr.raft, err = raft.NewRaft(config, mpr.sc, store, store, snapshotStore, transport)
+	//if err != nil {
+	//	return nil, errors.Wrap(err, "failed to create raft node")
+	//}
+	//
+	//if hasState, err := raft.HasExistingState(store, store, snapshotStore); err != nil {
+	//	return nil, errors.Wrap(err, "failed to check existing state")
+	//} else if !hasState {
+	//	configuration := raft.Configuration{
+	//		Servers: getRaftServers(raftBootstrapNodes),
+	//	}
+	//	future := mpr.raft.BootstrapCluster(configuration)
+	//	if err := future.Error(); err != nil {
+	//		return nil, errors.Wrap(err, "failed to create raft node")
+	//	}
+	//}
 
 	return mpr, nil
 }
 
-func getRaftServers(bootstrapNodes []string) []raft.Server {
-	servers := make([]raft.Server, len(bootstrapNodes))
-	for i, addr := range bootstrapNodes {
-		servers[i] = raft.Server{
-			ID:      raft.ServerID(addr),
-			Address: raft.ServerAddress(addr),
-		}
-	}
-	return servers
-}
+//
+//func getRaftServers(bootstrapNodes []string) []raft.Server {
+//	servers := make([]raft.Server, len(bootstrapNodes))
+//	for i, addr := range bootstrapNodes {
+//		servers[i] = raft.Server{
+//			ID:      raft.ServerID(addr),
+//			Address: raft.ServerAddress(addr),
+//		}
+//	}
+//	return servers
+//}
 
 func (mpr *metadataProviderRaft) Close() error {
-	return mpr.raft.Shutdown().Error()
+	return nil // mpr.raft.Shutdown().Error()
 }
 
 func (mpr *metadataProviderRaft) Get() (cs *model.ClusterStatus, version Version, err error) {
@@ -200,29 +195,30 @@ func (mpr *metadataProviderRaft) Store(cs *model.ClusterStatus, expectedVersion 
 	mpr.Lock()
 	defer mpr.Unlock()
 
-	if err = mpr.raft.VerifyLeader().Error(); err != nil {
-		return NotExists, err
-	}
+	//if err = mpr.raft.VerifyLeader().Error(); err != nil {
+	//	return NotExists, err
+	//}
+	//
+	//if mpr.sc.Version != expectedVersion {
+	//	panic(ErrMetadataBadVersion)
+	//}
+	//
+	//newState := &stateContainer{
+	//	ClusterStatus: cs.Clone(),
+	//	Version:       incrVersion(mpr.sc.Version),
+	//}
+	//
+	//serialized, err := json.Marshal(newState)
+	//if err != nil {
+	//	return NotExists, err
+	//}
+	//
+	//future := mpr.raft.Apply(serialized, 30*time.Second)
+	//if err := future.Error(); err != nil {
+	//	return NotExists, errors.Wrap(err, "failed to apply metadata provider")
+	//}
 
-	if mpr.sc.Version != expectedVersion {
-		panic(ErrMetadataBadVersion)
-	}
-
-	newState := &stateContainer{
-		ClusterStatus: cs.Clone(),
-		Version:       incrVersion(mpr.sc.Version),
-	}
-
-	serialized, err := json.Marshal(newState)
-	if err != nil {
-		return NotExists, err
-	}
-
-	future := mpr.raft.Apply(serialized, 30*time.Second)
-	if err := future.Error(); err != nil {
-		return NotExists, errors.Wrap(err, "failed to apply metadata provider")
-	}
-
-	mpr.sc = newState
-	return newState.Version, nil
+	//mpr.sc = newState
+	//return newState.Version, nil
+	return NotExists, nil
 }
--- go.mod
+++ go.mod
@@ -30,10 +30,7 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/google/uuid v1.6.0
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
-	github.com/hashicorp/raft v1.7.3
-	github.com/hashicorp/raft-boltdb v0.0.0-20250926130943-f41fa5f23d89
 	github.com/juju/fslock v0.0.0-20160525022230-4d5c94c67b4b
-	github.com/magodo/slog2hclog v0.0.0-20240614031327-090ebd72a033
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/oauth2-proxy/mockoidc v0.0.0-20240214162133-caebfff84d25
 	github.com/pkg/errors v0.9.1
@@ -68,9 +65,7 @@ require (
 
 require (
 	github.com/DataDog/zstd v1.5.2 // indirect
-	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cockroachdb/errors v1.12.0 // indirect
 	github.com/cockroachdb/fifo v0.0.0-20240606204812-0bbfbd93a7ce // indirect
@@ -79,7 +74,6 @@ require (
 	github.com/cockroachdb/tokenbucket v0.0.0-20250429170803-42689b6311bb // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/emicklei/go-restful/v3 v3.12.2 // indirect
-	github.com/fatih/color v1.13.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.8.0 // indirect
 	github.com/getsentry/sentry-go v0.32.0 // indirect
 	github.com/go-jose/go-jose/v3 v3.0.4 // indirect
@@ -93,12 +87,6 @@ require (
 	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/gnostic-models v0.6.9 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
-	github.com/hashicorp/go-hclog v1.6.3 // indirect
-	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
-	github.com/hashicorp/go-metrics v0.5.4 // indirect
-	github.com/hashicorp/go-msgpack v0.5.5 // indirect
-	github.com/hashicorp/go-msgpack/v2 v2.1.2 // indirect
-	github.com/hashicorp/golang-lru v0.5.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
